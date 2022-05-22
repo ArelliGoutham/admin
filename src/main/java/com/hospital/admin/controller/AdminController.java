cd868fe4d958f021a81baf8bd0fcf3e006700794
ArelliGoutham <arelligoutham@gmail.com>
2022-05-22 19:28:29 +0530

    v1.0 inital release

--- src/main/java/com/hospital/admin/controller/AdminController.java
+++ src/main/java/com/hospital/admin/controller/AdminController.java
@@ -7,10 +7,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class AdminController {
 
     @Autowired
